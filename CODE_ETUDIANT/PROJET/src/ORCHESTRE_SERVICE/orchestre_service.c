b6de913de1b66616eafc9a2bfe2e7c84206af385
Tigerwang <valdrinsalihi@hotmail.fr>
2021-12-07 16:25:51 +0100

    Orchestre_service.c

--- CODE_ETUDIANT/PROJET/src/ORCHESTRE_SERVICE/orchestre_service.c
+++ CODE_ETUDIANT/PROJET/src/ORCHESTRE_SERVICE/orchestre_service.c
@@ -36,33 +36,35 @@ int semCreation(int key, int initVal)
 }
 
 // À REMODIFIER ...
-void createFirstWorker(int pipeMasterWorker, int pipeWorkerMaster){
-	
-	//ordre des arguments : 
-	//  - nomExec
-	//	- primeNumber
-	//	- tube masterWorker
-	//	- tube workerMaster
-	char * c1 = intToString(pipeMasterWorker);
-	char * c2 = intToString(pipeWorkerMaster);
-	char * arguments[5] = { 
-		"worker",
-		"2",
-		c1,
-		c2,
-		NULL};
-		
-	int son = fork();
-	
-	//Fils créé ?
-	if (son == -1) {
-	   perror("createFirstWorker");
-	   exit(EXIT_FAILURE);
-	}
-	
-	if(son == 0){
-		execv("worker",arguments);
-	}
-	free(c1);
-	free(c2);
+void createFirstService(int pipeOrchestreService)
+{
+    
+    //ordre des arguments : 
+    //    - numéro du service
+    //    - clé du sémaphore
+    //    - "file descriptor du tube anonyme" orchestre vers service
+    //    - tube nommé de client vers service
+    //    - tube nommé de service vers client
+    int numService = 0;
+    int keySem = numService + 1;
+    char * c1 = intToString(pipeOrchestreService);
+    char * arguments[5] = { 
+        numService,
+        keySem,
+        c1,
+        pipe_c2s_0,
+        pipe_s2c_0};
+        
+    int son = fork();
+    
+    //Fils créé ?
+    if (son == -1) {
+       perror("createFirstWorker");
+       exit(EXIT_FAILURE);
+    }
+    
+    if(son == 0){
+        execv("service",arguments);
+    }
+    free(c1);
 }
