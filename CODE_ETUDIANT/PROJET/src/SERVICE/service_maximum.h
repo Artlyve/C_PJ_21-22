bbc7a56ef61c069b27f46334a014618d7c0efb25
Tigerwang <valdrinsalihi@hotmail.fr>
2021-12-09 11:58:15 +0100

    09/12

--- CODE_ETUDIANT/PROJET/src/SERVICE/service_maximum.h
+++ CODE_ETUDIANT/PROJET/src/SERVICE/service_maximum.h
@@ -4,7 +4,6 @@
 // on ne déclare ici que les fonctions appelables par le main
 
 // fonction pour gérer la communication avec le client
-void service_maximum(/* fd des deux tubes nommés avec le client */);
+void service_maximum(int fdSC, int fdCS);
 
 #endif
-
